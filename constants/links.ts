b0dbe6c10fb660a27e424866907aec656c62cc91
Your Name <stancikmarian8@gmail.com>
2025-03-12 13:13:08 +0100

    call integration

--- constants/links.ts
+++ constants/links.ts
@@ -1,8 +1,8 @@
 export const CONSTANTS = {
   LOGIN_LINK: "/login",
-  CALCOM_NAMESPACE: "chat-with-ABRA-demo",
+  CALCOM_NAMESPACE: "15min",
   CALCOM_BRAND_COLOR: "#000000",
   CALCOM_HIDE_EVENT_TYPE_DETAILS: false,
   CALCOM_LAYOUT: "month_view",
-  CALCOM_LINK: "manu-arora-vesr9s/chat-with-manu-demo",
+  CALCOM_LINK: "abra-ai/15min",
 };
